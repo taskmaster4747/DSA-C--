bff1815b95c4cc135839e345beba0cdb10acc0be
taskmaster4747 <taskmaster4747@gmail.com>
2026-01-25 10:56:56 +0530

    update code

---
+++ code_6.cpp
@@ -0,0 +1,20 @@
+//Even odd number using if else statemant
+
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int n;
+
+    cout<<"Enter a Number : "<<endl;
+    cin>>n;
+
+    if(n%2 == 0){
+        cout<<"even\n";
+    }else{
+        cout<<"odd\n";
+    }
+
+    return 0;
+}
