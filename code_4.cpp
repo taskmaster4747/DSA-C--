2b6854035eb6e0433af39be81e07b2a806b32e7b
taskmaster4747 <taskmaster4747@gmail.com>
2026-01-25 10:48:17 +0530

    update code

---
+++ code_4.cpp
@@ -0,0 +1,18 @@
+//if else statemane
+
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int n = 45;
+
+    if(n >= 0){
+      cout<<"n is Positive"<<endl;
+    }
+    else{
+      cout<<"n is Negative"<<endl;
+    }
+
+    return 0;
+}
---
+++ code_5.cpp
@@ -0,0 +1,21 @@
+//if else statemane
+
+#include<iostream>
+using namespace std;
+
+int main(){
+
+   int age;
+
+   cout<<"Enter the age : "<<endl;
+   cin>>age;
+
+   if(age >= 18){
+     cout<<"You can vote "<<endl;
+   } 
+   else{
+    cout<<"You can't vote "<<endl;
+   }
+
+    return 0;
+}
