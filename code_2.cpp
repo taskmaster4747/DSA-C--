de538723718ec32908712019214bc1d5f253966a
taskmaster4747 <taskmaster4747@gmail.com>
2026-01-25 09:40:25 +0530

    update code

---
+++ code_2.cpp
@@ -0,0 +1,12 @@
+//Operators
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    
+   cout << ( ( 3 < 1 ) || ( 3 < 5 ) ) << endl;  // OR Operator
+   cout << ( ( 3 < 1 ) && ( 3 < 5 ) ) << endl;  // AND Operator
+    
+    return 0;
+}
