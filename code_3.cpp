39bbc81c24dceb4801c98dc2dc73ef72407ac625
taskmaster4747 <taskmaster4747@gmail.com>
2026-01-25 09:49:10 +0530

    update code

---
+++ code_3.cpp
@@ -0,0 +1,20 @@
+// input out put and sum of two numbers
+
+#include<iostream>
+using namespace std;
+
+int main(){
+   
+    int A , B;
+    
+    cout<<"Enter Value of A : "<<endl;
+    cin>>A;
+
+    cout<<"Enter Value of B "<<endl;
+    cin>>B;
+
+    int sum = A+B;
+    cout<<"Sum of A and B is : "<<sum<<endl;    
+
+    return 0;
+}
